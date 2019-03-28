b938ae43ae5cc7bf9582f6becf95678e9889ab7b
John Forster <JohnnyFlight@users.noreply.github.com>
2019-03-28 23:06:02 +0000

    Added Bezier Curve Approximation. Started Pi Collisions.

---
+++ Bezier Curve Approximation/index.js
@@ -0,0 +1,110 @@
+const height = 600;
+const width = 600;
+
+const topEdge = 100;
+const leftEdge = 100;
+
+window.onload = () =>
+{
+  //  Setup event listeners
+  document.getElementById('steps').onchange = updateView;
+  document.getElementById('minus10').onclick = () => changeSteps(-10);
+  document.getElementById('minus1').onclick = () => changeSteps(-1);
+  document.getElementById('plus1').onclick = () => changeSteps(1);
+  document.getElementById('plus10').onclick = () => changeSteps(10);
+
+  loadParametersFromLocalStorage();
+
+  updateView();
+}
+
+function changeSteps(value)
+{
+  var elem = document.getElementById('steps');
+  var steps = parseInt(elem.value);
+  steps += value;
+
+  if (steps < elem.min) steps = elem.min;
+  if (steps > elem.max) steps = elem.max;
+
+  elem.value = steps;
+  elem.onchange();
+}
+
+function loadParametersFromLocalStorage()
+{
+  var parameters = JSON.parse(localStorage.parameters);
+
+  for (var param of Object.keys(parameters))
+  {
+    var elem = document.getElementById(param);
+    if (elem)
+    {
+      elem.value = parameters[param];
+    }
+  }
+}
+
+function updateView()
+{
+  draw(getParameters());
+}
+
+function getParameters()
+{
+  var params = {};
+
+  params.steps = parseInt(document.getElementById('steps').value);
+
+  localStorage.parameters = JSON.stringify(params);
+
+  return params;
+}
+
+function draw(params)
+{
+  var ctx = document.getElementById('canvas').getContext('2d');
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  //  Draw axes
+  ctx.beginPath();
+
+  ctx.moveTo(topEdge, leftEdge);
+  ctx.lineTo(leftEdge, topEdge + height);
+  ctx.lineTo(leftEdge + width, topEdge + height);
+  ctx.stroke();
+
+  ctx.closePath();
+
+
+  for (var i = 0; i < params.steps; i++)
+  {
+    ctx.beginPath();
+    ctx.strokeStyle = '1px solid black';
+    ctx.fillStyle = 'red';
+    //  Draw points
+    ctx.arc(leftEdge, topEdge + height - (height / params.steps) * (i + 1), 5, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.closePath();
+
+    ctx.beginPath();
+    //  Draw points
+    ctx.arc(leftEdge + width - (width / params.steps) * (i), topEdge + height, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.closePath();
+
+    //  Draw lines
+    ctx.beginPath();
+
+    ctx.moveTo(leftEdge, topEdge + height - (height / params.steps) * (i + 1));
+    ctx.lineTo(leftEdge + width - (width / params.steps) * (i), topEdge + height);
+    ctx.stroke();
+
+    ctx.closePath();
+  }
+
+}
